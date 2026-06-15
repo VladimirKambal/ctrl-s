d76b4ea33c31cf70bf8d9c61e9d7478727e69a8b
VladimirKambal <ovsepancilia@gmail.com>
2026-06-15 17:37:00 +0300

    FIX:Make_function_work_with_bat_file

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import keyboard
 import subprocess
+import os
+import tempfile
 
 REPO_PATH = r"C:\Users\thinzano\Desktop\git project\project 3"
 
-def open_git_commit():
-    cmd = (
-        f'cd /d "{REPO_PATH}" && '
-        f'git add . && '
-        f'set /p MSG=">> Название коммита: " && '
-        f'git commit -m "%MSG%" && '
-        f'git push && '
-        f'echo. && echo Готово! && pause > nul'
-    )
+def git_commit():
+    bat_content = f"""@echo off
+cd /d "{REPO_PATH}"
+git add .
+set /p MSG=">> Название коммита: "
+git commit -m "%MSG%"
+git push
+echo.
+echo Готово!
+pause
+"""
+    bat_path = os.path.join(tempfile.gettempdir(), "git_commit.bat")
+    with open(bat_path, "w", encoding="cp866") as f:
+        f.write(bat_content)
+
     subprocess.Popen(
-        ["cmd.exe", "/K", cmd],
+        ["cmd.exe", "/K", bat_path],
         creationflags=subprocess.CREATE_NEW_CONSOLE
     )
-
 print("Запущено! Нажми Ctrl+S для создания коммита\n(ПРОВЕРЬ! Правильно ли указан путь, а так же запущена ли консоль от имени администратора)")
-keyboard.add_hotkey('ctrl+s', open_git_commit)
+keyboard.add_hotkey('ctrl+s', git_commit)
 keyboard.wait() 
